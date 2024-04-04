5af72d916d2a55916ecff56e85d778361bc4003a
Jairik <jairikmc@gmail.com>
2024-04-03 22:58:21 -0400

    Initial Commit
    Added some templates and other stuff, just trying to figure out GUI development.

---
+++ Java/Model.java
@@ -0,0 +1,14 @@
+/* Model Class
+ *  Creation Date: 4/3/24
+ *  Last Update: 4/3/24
+ *  Description: --- */
+
+class Model {
+    //variables here
+
+    //Model Contructor
+    public Model() {
+        //Logic here
+    }
+
+}
--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # MOVE4WELLNESS-Android-App
 Android app for the MOVE4WELLNESS android app.
-
-Start Date: 4/2/24
-
-Due Date: 5/14/24
-
-Current Steps: Sketching GUI & Planning
