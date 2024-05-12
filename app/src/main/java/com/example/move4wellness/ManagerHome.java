06c1f938de0e6139679691c72db5389e5c944b47
Jairik <jairikmc@gmail.com>
2024-05-12 03:41:27 -0400

    "added" progressbar
    Something is insanely broken as to where it won't work. womp womp.

--- app/src/main/java/com/example/move4wellness/ManagerHome.java
+++ app/src/main/java/com/example/move4wellness/ManagerHome.java
@@ -7,6 +7,7 @@ package com.example.move4wellness;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,6 +23,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Objects;
+
 public class ManagerHome extends AppCompatActivity {
 
     TextView totalUsersText;
@@ -29,6 +32,7 @@ public class ManagerHome extends AppCompatActivity {
     TextView allTimeAverageText;
     TextView allTimeActivitiesText;
     TextView allTimeAverageActivitiesText;
+    ProgressBar progressBar;
     FirebaseFirestore db;
     CollectionReference usersRef;
 
@@ -42,10 +46,13 @@ public class ManagerHome extends AppCompatActivity {
         allTimeAverageText = findViewById(R.id.all_Time_Average);
         allTimeActivitiesText = findViewById(R.id.total_activities);
         allTimeAverageActivitiesText = findViewById(R.id.average_activities);
+        //Getting the loading bar
+        progressBar = findViewById(R.id.progress_bar);
         //Setting up database variable
+        progressBar.setVisibility(View.VISIBLE);
         db = FirebaseFirestore.getInstance();
         usersRef = db.collection("users");
-        //Setting all the text-fields
+        //Pulling data from the database to edit the statistical text fields
         setTextFields();
     }
 
@@ -70,20 +77,25 @@ public class ManagerHome extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if(task.isSuccessful()) {
+                    //For all documents in the users collection
                     for(QueryDocumentSnapshot doc : task.getResult()) {
-                        Integer currentExercises = doc.getLong("num_exercises").intValue();
-                        Integer currentTotalDuration = doc.getLong("total_minutes").intValue();
+                        //Get the statistics, and cast them to ints
+                        long currentExercisesL = doc.getLong("num_exercises");
+                        int currentExercises = (int) currentExercisesL;
+                        long currentTotalDurationL = doc.getLong("total_minutes");
+                        int currentTotalDuration = (int) currentTotalDurationL;
 
                         numUsers[0] += 1; //Incrementing by one
                         numExercises[0] += currentExercises; //Incrementing total exercises
                         totalDuration[0] += currentTotalDuration; //Incrementing total Duration of exercises
                     }
+
                     //Setting the strings to replace in the text-fields
                     String numUsersString = String.valueOf(numUsers[0]);
                     String totalDurationString = String.valueOf(totalDuration[0]) + " minutes";
-                    String averageDurationString = String.valueOf((totalDuration[0])/numUsers[0]) + " minutes";
-                    String totalExercises = String.valueOf(totalDuration[0]);
-                    String averageTotalExercises = String.valueOf((totalDuration[0])/numUsers[0]);
+                    String averageDurationString = String.valueOf((totalDuration[0])/numUsers[0]) + " minutes per user";
+                    String totalExercises = String.valueOf(numExercises[0]);
+                    String averageTotalExercises = String.valueOf((numExercises[0])/numUsers[0]) + " per user";
 
                     //Replacing edit-text fields
                     totalUsersText.setText(numUsersString);
@@ -92,12 +104,9 @@ public class ManagerHome extends AppCompatActivity {
                     allTimeActivitiesText.setText(totalExercises);
                     allTimeAverageActivitiesText.setText(averageTotalExercises);
                 }
-                else {
-                    totalUsersText.setText("ERROR - Try Again");
-                }
             }
         });
-
+        progressBar.setVisibility(View.INVISIBLE); //Hiding the progressbar when operation is complete
     }
 
 
