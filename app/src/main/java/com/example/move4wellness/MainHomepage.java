f136b89483d91214cace7996714e115c0eea29b8
Will-lamuth <156252007+Will-lamuth@users.noreply.github.com>
2024-05-12 17:20:44 -0400

    updated main homepage
    -username updates with user

--- app/src/main/java/com/example/move4wellness/MainHomepage.java
+++ app/src/main/java/com/example/move4wellness/MainHomepage.java
@@ -6,19 +6,81 @@ package com.example.move4wellness;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainHomepage extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    FirebaseUser user;
+    FirebaseFirestore db;
+    DocumentReference userRef;
+    String uniqueUserID;
+    String username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_homepage);
+        //Getting the current user
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        /*Getting the current userUID, getting the database, and establishing a reference to the
+         * current user in the 'user' collection */
+        if(user != null) {
+            uniqueUserID = user.getUid();
+            userRef = db.collection("users").document(uniqueUserID);
+        }
+        else { //No user is signed in, redirect to main activity
+            noUserDetected(); //Go back to first welcome screen
+        }
+
+        //grab the username from the user document
+        if (userRef != null) {
+            userRef.get()
+                    .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if (documentSnapshot.exists()) {
+                                // Document exists, fetch username
+                                username = documentSnapshot.getString("username");
+                            }
+                            //set text to user's username
+                            TextView name = findViewById(R.id.username);
+                            name.setText(username);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            System.out.println("Error getting document");
+                        }
+                    });
+        } else {
+            System.out.println("userRef is null");
+        }
+
+    }
+
+    //If there is somehow no user signed in, will redirect to login/register page
+    private void noUserDetected() {
+        Intent intent = new Intent(MainHomepage.this, MainActivity.class);
     }
 
     //redirects to noti screen
