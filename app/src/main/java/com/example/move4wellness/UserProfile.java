40f30ea81a845dc0a54f1bedfaba07019a827ba8
Will-lamuth <156252007+Will-lamuth@users.noreply.github.com>
2024-05-13 16:20:26 -0400

    Update activity takes date
    - When user wants to track an exercise they manually input a date(MM/DD/YYYY)

--- app/src/main/java/com/example/move4wellness/UserProfile.java
+++ app/src/main/java/com/example/move4wellness/UserProfile.java
@@ -3,19 +3,61 @@ package com.example.move4wellness;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class UserProfile extends AppCompatActivity {
 
+    FirebaseAuth auth;
+    FirebaseUser user;
+    TextView usernameText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile);
+        usernameText = findViewById(R.id.username_profile);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        setUsernameText();
+    }
+
+    private void setUsernameText() {
+        user = auth.getCurrentUser();
+        //If the user is null (it shouldn't be), return false
+        if(user == null) {
+            return;
+        }
+        //Getting details of the current user & the database
+        String UID = user.getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference userRef = db.collection("users").document(UID);
+
+        userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    if(doc.exists()) {
+                        String userName = doc.getString("username");
+                        usernameText.setText(userName);
+                    }
+                }
+            }
+        });
     }
 
     //method sends back to first login/register screen
